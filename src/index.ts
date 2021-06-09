023caa06120dcdf19c458e501220789baff0762f
Louis Gallet <louis.gallet@tumo.world>
2021-06-09 21:33:18 +0200

    feat: :construction: Starting create hash web

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ const port = 8000
 app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
 
+app.use('/static', express.static(__dirname + '/views'));
+
 app.get('/', (req, res) => {
     res.render( "index" );
 })
---
+++ src/views/js/main.ts
@@ -0,0 +1,4 @@
+function hashString(form: any) {
+    // tslint:disable-next-line:no-console
+    console.log(form.hashed_text.value)
+}
